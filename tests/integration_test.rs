8f6f94ba535a1d5b02ded62fc2564387f12a1cb8
Reid Lai <reidlai@codelity.co>
2024-01-27 09:29:32 -0500

    move all modules to library crate instead of binary crate in order to allow integration test importing all modules under this project

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -5,11 +5,12 @@ extern crate rspec;
 #[macro_use]
 extern crate hamcrest;
 
-use std::{io};
-use std::sync::{Arc};
-
 mod cpu;
 mod mpi;
+
+use std::{io};
+use std::sync::{Arc};
+use cp631_final::technical_analysis;
 
 pub fn main() {
   let logger = Arc::new(rspec::Logger::new(io::stdout()));
@@ -36,6 +37,28 @@ pub fn main() {
 
       ctx.it("MPI must be able to initialize", |_| {
         mpi::test_mpi_initialization();
+      });
+
+    });
+
+    // EMA function testing
+    ctx.specify("EMA function testing", |ctx| {
+
+      ctx.it("EMA function must return correct value", |_| {
+        let ema_values = technical_analysis::exponentail_moving_average(2, vec![1.0, 2.0, 3.0]);
+        assert_ne!(ema_values, 2.0);
+        assert_eq!(ema_values, 2.5555555555555554);
+      });
+
+    });
+
+    // RSI function testing
+    ctx.specify("RSI function testing", |ctx| {
+
+      ctx.it("RSI function must return correct value", |_| {
+        let rsi_value = technical_analysis::relative_strength_index(2, vec![1.0, 2.0, 3.0]);
+        assert_ne!(rsi_value, 98.0);
+        assert_eq!(rsi_value, 98.78048780487805);
       });
 
     });
